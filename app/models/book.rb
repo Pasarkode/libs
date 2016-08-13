645aa6df944d60c5a8805d7f4922e35035c46aaa
Taufan <andadj@gmail.com>
2016-08-13 11:58:29 +0700

    Add Data Validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,12 @@
 class Book < ActiveRecord::Base
 #  attr_accessible :image
   mount_uploader :image, ImageUploader
-  
+
   has_many :reviews
+
+  validates :title, :description, :year, :author, :publisher, :pages, :image, presence: true
+  validates :year, format: { with: /\d{4}/,
+    message: "only allows format eg: 2016" }
+  validates :pages, format: { with: /\d/,
+    message: "only allows numbers" }
 end
