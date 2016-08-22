836c7f7ae1643adfe846fff3f476958064de066a
Taufan <andadj@gmail.com>
2016-08-22 23:27:19 +0700

    Membuat user hanya bisa mereview 1 kali dan memperbaiki fungsi update review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
-
+  validates :user_id, :uniqueness => { :scope => :book_id,
+    :message => " Hanya Dapat Mereview 1 Kali silahkan Edit atau Hapus terlebih dahulu!" }
   validates :rating, :comment, presence: true
   validates :rating, numericality: {
     only_integer: true,
